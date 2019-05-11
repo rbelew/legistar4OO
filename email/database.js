fe7b755d1f861435de9f6d472ead0854d9a40d21
in hyub jeon <inhyub@inhyubs-MacBook-Air.local>
2019-05-11 14:41:19 -0700

    updated database schema

--- email/database.js
+++ email/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/emails', {useNewUrlParser: true});
 
-const citySchema = new mongoose.Schema({
+const bodySchema = new mongoose.Schema({
   city: String,
-  bodies: [{body: String, emails: [String]}]
+  body: String,
+  emails: [String]
 });
 
-const City = mongoose.model('City', citySchema);
+const Body = mongoose.model('Body', bodySchema);
