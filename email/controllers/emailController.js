7774a621108d3cc8f933f9788fa6322a8430d32c
Inventedforms <artifion@gmail.com>
2019-05-11 14:55:10 -0700

    more boilerplate

---
+++ email/controllers/emailController.js
@@ -0,0 +1,2 @@
+const auth = require('../security/auth.js')
+app.post('/emails', (req, res) => res.send('Hello World!'))
--- email/security/auth.js
+++ email/security/auth.js
@@ -1,3 +1,10 @@
+
+const config = require('./config/config.json');
+
+const bcrypt = requre('bcrypt');
+
 function authenticate(body, signature){
-    
+    var bodyString = JSON.stringify(body);
+    const value = bcrypt.compare(bcrypt.hash(bodyString + config.hash_secret), signature);
+    return value;
 }
